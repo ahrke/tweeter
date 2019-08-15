24f459802f3409bc355f3f32008900a00009ecb2
ahrke <david.duy.minh@gmail.com>
2019-08-15 16:37:03 +0000

    fixed animation, and counter bugs

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,9 +1,9 @@
 // Re: New Tweet Form. We need a way to count the amount of characters user has input, from there we need to render the value to an element (.counter)
 // We also need to change the color of .counter when the user has exceed 140 characters
 
-$('.newTweetTextArea').on("keydown", () => {
-  let count = $('.newTweetTextArea').val().length;
-  (count > 140) 
+$('.newTweetTextArea').on("keyup", () => {
+  let count = 140 - $('.newTweetTextArea').val().length;
+  (count < 0) 
     ? $('.counter').css('color', 'red')
     : $('.counter').css('color', '#fcf9ea')
   $('.counter').text(count);
